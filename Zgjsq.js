ab515e1d7a151b31beb6b61a296986bd1d135dab
to889 <funky.exciter-06@icloud.com>
2024-12-18 23:41:55 +0800

    Update Zgjsq.js

--- Zgjsq.js
+++ Zgjsq.js
@@ -1,21 +1,24 @@
 #!name=建工计算器
 
 [Script]
-^https?:\/\/calc\.kuaicad\.com\/authority\/verify_vip url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/jgjsq.js
+# 建工计算器 VIP 解锁
+http-response ^https?:\/\/calc\.kuaicad\.com\/authority\/verify_vip script-path=https://raw.githubusercontent.com/chxm1023/Rewrite/main/jgjsq.js, requires-body=true, tag=建工计算器VIP解锁
 
 [Mitm]
 hostname = calc.kuaicad.com
 
-*************************************/
-
-
+/**
+ * 建工计算器 VIP 解锁
+ */
 var chxm1023 = JSON.parse($response.body);
 
 chxm1023.data = {
   ...chxm1023.data,
-  "type" : 1,
-  "expiresTime" : 4092599349000,
-  "isExpires" : false
+  "type": 1,
+  "expiresTime": 4092599349000,
+  "isExpires": false
 };
 
-$done({body : JSON.stringify(chxm1023)});
+$done({body: JSON.stringify(chxm1023)});
+
+
