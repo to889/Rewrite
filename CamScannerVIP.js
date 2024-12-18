fad0662c3bce1aa38baa23b7a061f1f2baed2db1
to889 <funky.exciter-06@icloud.com>
2024-12-18 23:29:05 +0800

    Update CamScannerVIP.js

--- CamScannerVIP.js
+++ CamScannerVIP.js
@@ -1,4 +1,5 @@
 #!name = 扫描全能王VIP
+#!icon=https://raw.githubusercontent.com/deezertidal/private/main/icons/cs.png
 
 [Script]
 http-response ^https:\/\/.*\.(intsig\.net|camscanner\.com)\/(purchase\/cs\/query_property|queryProperty|getPrivilegeItem) script-path=https://raw.githubusercontent.com/chxm1023/Rewrite/main/CamScanner.js, requires-body=true, timeout=10, tag=CamScanner VIP Unlock
