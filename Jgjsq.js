373acda99157774a654f67be7bec37b7ed038afa
to889 <funky.exciter-06@icloud.com>
2025-01-10 14:43:05 +0800

    Update Jgjsq.js

--- Jgjsq.js
+++ Jgjsq.js
@@ -2,7 +2,7 @@
 #!icon=https://raw.githubusercontent.com/to889/Rewrite/refs/heads/Loon/Jgjsq.png
 
 [Script]
-http-response ^https?:\/\/calc\.kuaicad\.com\/authority\/verify_vip script-path=https://raw.githubusercontent.com/chxm1023/Rewrite/main/jgjsq.js, requires-body=true, tag=建工计算器VIP解锁
+http-response ^https?:\/\/calc\.kuaicad\.com\/authority\/verify_vip script-path=https://raw.githubusercontent.com/to889/Rewrite/refs/heads/Loon/jgjsq.js, requires-body=true, tag=建工计算器VIP解锁
 
 [Mitm]
 hostname = calc.kuaicad.com
