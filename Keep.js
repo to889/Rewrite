3f155e584bf01d6a533df0c87c64eedff445fe75
to889 <funky.exciter-06@icloud.com>
2025-01-10 14:46:03 +0800

    Update Keep.js

--- Keep.js
+++ Keep.js
@@ -2,7 +2,7 @@
 #!icon=https://raw.githubusercontent.com/to889/Rewrite/refs/heads/Loon/Keep.png
 
 [Script]
-http-response ^https?:\/\/(api|kit).gotokeep\.com\/(nuocha|gerudo|athena|nuocha\/plans|suit\/v5\/smart|kprime\/v4\/suit\/sales)\/ script-path=https://raw.githubusercontent.com/WeiGiegie/666/main/keep.js, requires-body=true, timeout=10
+http-response ^https?:\/\/(api|kit).gotokeep\.com\/(nuocha|gerudo|athena|nuocha\/plans|suit\/v5\/smart|kprime\/v4\/suit\/sales)\/ script-path=https://raw.githubusercontent.com/to889/Rewrite/refs/heads/Loon/keep.js, requires-body=true, timeout=10
 
 [MITM]
 hostname = *.gotokeep.com, 162.14.5.*, 42.187.199.*, 101.42.124.*
