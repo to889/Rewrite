1282578c0cb25809a2f09bdee467123aa5645ff6
to889 <funky.exciter-06@icloud.com>
2025-02-28 03:04:20 +0800

    Update FT.js

--- FT.js
+++ FT.js
@@ -2,22 +2,39 @@
 #!icon=https://raw.githubusercontent.com/to889/Rewrite/refs/heads/Loon/FT.png
 
 [Script]
-http-response ^https:\/\/.*\.cloudfront\.net\/index\.php\/jsapi\/paywall script-path=https://raw.githubusercontent.com/Yu9191/Rewrite/main/FTzhongwenwang.js, requires-body=true, timeout=10, tag=FT中文网
+# 拦截 FT 中文网的开屏广告和解锁会员
+http-response ^https:\/\/.*\.cloudfront\.net\/index\.php\/jsapi\/paywall$ script-path=https://raw.githubusercontent.com/user/loon_ft_ads.js, requires-body=true, timeout=10, tag=FT中文网
+http-response ^https:\/\/www\.ftchinese\.com\/index\.html$ script-path=https://raw.githubusercontent.com/user/loon_ft_ads.js, requires-body=true, timeout=10
 
 [Mitm]
-hostname = *.cloudfront.net
+hostname = *.cloudfront.net, *.ftchinese.com, *.ftimg.net
 
-var baby = JSON.parse($response.body);
 
-baby = {
-  "paywall": 0,
-  "premium": 1,
-  "expire": "4092599349",
-  "standard": 1,
-  "v": 2099,
-  "campaign_code": "",
-  "latest_duration": "yearly",
-  "addon": 0
-};
+let body = $response.body;
 
-$done({ body: JSON.stringify(baby) });
+try {
+    // 处理 paywall 解锁
+    if ($request.url.includes("/index.php/jsapi/paywall")) {
+        let baby = JSON.parse(body);
+        baby = {
+            "paywall": 0,
+            "premium": 1,
+            "expire": "4092599349",
+            "standard": 1,
+            "v": 2099,
+            "campaign_code": "",
+            "latest_duration": "yearly",
+            "addon": 0
+        };
+        body = JSON.stringify(baby);
+    }
+    
+    // 去除开屏广告
+    if ($request.url.includes("/index.html")) {
+        body = body.replace(/<div class="ad-container">.*?<\/div>/g, "");
+    }
+} catch (e) {
+    console.log("FT中文网脚本错误：" + e);
+}
+
+$done({ body });
