3e13bffca7a9af3b7b5749ea34ac4cb8de5c5290
to889 <funky.exciter-06@icloud.com>
2025-02-28 03:07:35 +0800

    Update FT.js

--- FT.js
+++ FT.js
@@ -2,7 +2,6 @@
 #!icon=https://raw.githubusercontent.com/to889/Rewrite/refs/heads/Loon/FT.png
 
 [Script]
-# 拦截 FT 中文网的开屏广告和解锁会员
 http-response ^https:\/\/.*\.cloudfront\.net\/index\.php\/jsapi\/paywall$ script-path=https://raw.githubusercontent.com/Yu9191/Rewrite/main/FTzhongwenwang.js, requires-body=true, timeout=10, tag=FT中文网
 http-response ^https:\/\/www\.ftchinese\.com\/index\.html$ script-path=https://raw.githubusercontent.com/Yu9191/Rewrite/main/FTzhongwenwang.js, requires-body=true, timeout=10
 
@@ -13,7 +12,6 @@ hostname = *.cloudfront.net, *.ftchinese.com, *.ftimg.net
 let body = $response.body;
 
 try {
-    // 处理 paywall 解锁
     if ($request.url.includes("/index.php/jsapi/paywall")) {
         let baby = JSON.parse(body);
         baby = {
@@ -29,7 +27,6 @@ try {
         body = JSON.stringify(baby);
     }
     
-    // 去除开屏广告
     if ($request.url.includes("/index.html")) {
         body = body.replace(/<div class="ad-container">.*?<\/div>/g, "");
     }
