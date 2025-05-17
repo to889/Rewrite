f9fc10c13cf777b75bfac8eac3c309bab8350c39
to889 <funky.exciter-06@icloud.com>
2025-05-17 08:16:32 +0800

    Update and rename Sam's.js to Sam.js

--- Sam.js
+++ Sam.js
@@ -1,4 +1,5 @@
 #!name = Sam's
+#!icon=https://raw.githubusercontent.com/to889/Rewrite/refs/heads/Loon/Sam.png
 
 [Rewrite]
 ^https?:\/\/api-sams\.walmartmobile\.cn\/api\/v\d\/sams\/sams-user\/(window\/getGoUpPlus|screen_promotion\/get) - reject
